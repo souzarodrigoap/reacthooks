2cb7761cd8da92b6df4e3280e02f64cf619a2758
rsouza98 <ro-marilia@outlook.com>
2020-12-09 22:34:51 -0300

    Andamento aula

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -10,24 +10,31 @@ export default function App() {
     setRepositories(data);
   }, []);
 
-  function handleAddRepository() {
-    setRepositories([ 
-      ...repositories, 
-      { id: Math.random(), name:"novo-repo"}
-    ]);
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} favoritos`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+
+    setRepositories(newRepositories);
   }
 
   return (
     <>
       <ul>
         { repositories.map ( repo => (
-          <li key={repo.id}>{repo.name}</li>
+          <li key={repo.id}>
+            {repo.name} 
+            { repo.favorite && <span>(Favorito)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar repo</button>
+          </li>
         ))}
       </ul>
-
-      <button onClick={handleAddRepository}>
-        Adicionar repositório
-      </button>
     </>
   )
 }
